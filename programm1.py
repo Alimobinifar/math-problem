534d935d6ab303a4b119492a9a34f38ba9ede203
alimobinifar <mobinifarali62@gmail.com>
2021-11-14 13:03:32 +0330

    13:03

--- programm1.py
+++ programm1.py
@@ -1,5 +1,23 @@
 list1=[] 
 list2=[]
+def merge_lists(n,x):
+    print(n+x)
+
+def fill_list2():
+    x=input('please enter an charachter or number  for list 2 : ')
+    list2.append(x)
+    while True:
+        print('Thank you '+'if you wanna continue filling first list please enter {}'+'\n'+
+        'if you wanna finish the operation {}'.format('continue','finish')
+        )
+        varibale_decision=input()
+        if varibale_decision =='continue':
+            new_variable=input('ok enter your value:')
+            list1.append(new_variable)
+        elif varibale_decision=='finish':
+            merge_lists(list1, list2)
+            break
+    print(list2)
 def fill_list1():
     x=input('please enter an charachter or number  : ')
     list1.append(x)
@@ -17,19 +35,3 @@ def fill_list1():
 
     print(list1)
 fill_list1()
-def fill_list2():
-    x=input('please enter an charachter or number  for list 2 : ')
-    list2.append(x)
-    while True:
-        print('Thank you '+'if you wanna continue filling first list please enter {}'+'\n'+
-        'if you wanna finish the operation{}'.format('continue','finish')
-        )
-        varibale_decision=input()
-        if varibale_decision =='continue':
-            new_variable=input('ok enter your value:')
-            list1.append(new_variable)
-        elif varibale_decision=='next':
-            break
-    print(list2)
-
-    
